3d00b87692be980437e9d81022d4417735552dc3
Karol <karol.gratus@gmail.com>
2018-01-31 13:58:10 +0100

    home and contact components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import App from './containers/App';
+import Navigation from './presentational/Navigation';
+
 import registerServiceWorker from './registerServiceWorker';
+
+const FourOhFour = () => <h1>404: Page not found!</h1>;
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route patch="/" component={Navigation} />
+        <hr />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 registerServiceWorker();
