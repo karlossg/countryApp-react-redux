f15f47f2621e25e595b9c035c08fb218800ed555
Karol <karol.gratus@gmail.com>
2018-01-31 15:10:05 +0100

    contry details

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,17 +1,21 @@
-import { GET_COUNTRIES } from '../actions/countries-actions';
+import { GET_COUNTRIES, GET_COUNTRY } from '../actions/countries-actions';
 import countriesData from '../data/countries.json';
 
 const initialState = {
-    countries: countriesData
+  countries: countriesData,
+  selectedCountry: {}
 };
 
-const countriesReducer = function (state = initialState, action) {
-    switch (action.type) {
-        case GET_COUNTRIES:
-            return Object.assign({}, state, { countries: state.countries })
-    }
+const countriesReducer = function(state = initialState, action) {
+  switch (action.type) {
+    case GET_COUNTRIES:
+      return Object.assign({}, state, { countries: state.countries });
+    case GET_COUNTRY:
+      const selectedCountry = state.countries.find(country => country.id === parseInt(action.id));
+      return Object.assign({}, state, { selectedCountry });
+  }
 
-    return state;
+  return state;
 };
 
 export default countriesReducer;
