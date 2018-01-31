3507e3e620644d88979fdd2995c7e8581488d414
Karol <karol.gratus@gmail.com>
2018-01-31 14:43:11 +0100

    flag container

---
+++ src/containers/CountryFlagContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CountryFlagList from '../presentational/CountryFlagList';
+import { getCountries } from '../actions/countries-actions';
+
+class CountryFlagContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getCountries());
+  }
+
+  render() {
+    return (
+      <div>
+        <CountryFlagList countries={this.props.countries} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = function(store) {
+  return {
+    countries: store.countriesReducer.countries
+  };
+};
+
+export default connect(mapStateToProps)(CountryFlagContainer);
